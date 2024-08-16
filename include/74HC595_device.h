57976807b34dc6f6db2ebcc2cbfa779665c3b110
康智超 <kangzhichao@3dbox.com>
2024-08-16 17:58:23 +0800

    一个芯片实现了IO的反转

--- include/74HC595_device.h
+++ include/74HC595_device.h
@@ -2,6 +2,9 @@
 #define __74HC959_DEVICE__
 #include <ShiftRegister74HC595.h>
 
-
+extern ShiftRegister74HC595<1> O1_74HC595_sr;
+extern ShiftRegister74HC595<1> O2_74HC595_sr;
+extern ShiftRegister74HC595<1> O3_74HC595_sr;
+extern ShiftRegister74HC595<1> O4_74HC595_sr;
 
 #endif // !__74HC959_DEVICE__
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,35 @@
-#include <Arduino.h>
+#include "74HC595_device.h"
 
 void setup()
 {
-
   Serial.begin(115200);
-  Serial.println("Hello World!");
+  //设置PA1为低电平
+  pinMode(PA1,OUTPUT);
+  digitalWrite(PA1,LOW);
 }
 
 void loop()
 {
-  Serial.println("loop!");
-  delay(1000);
+
+  // setting all pins at the same time to either HIGH or LOW
+  O1_74HC595_sr.setAllHigh(); // set all pins HIGH
+  {
+    int pin = 0;
+    uint8_t state = O1_74HC595_sr.get(pin); // 0 = LOW, 1 = HIGH
+    Serial.printf("Pin %d is %d\n", pin, state);
+  }
+    O1_74HC595_sr.updateRegisters(); // update the pins to the set values
+
+  delay(10);
+
+  O1_74HC595_sr.setAllLow(); // set all pins LOW
+  {
+    int pin = 0;
+    uint8_t state = O1_74HC595_sr.get(pin); // 0 = LOW, 1 = HIGH
+    Serial.printf("Pin %d is %d\n", pin, state);
+  }
+    O1_74HC595_sr.updateRegisters(); // update the pins to the set values
+
+  delay(10);
+
 }
