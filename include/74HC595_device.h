986e343333add0b252647d3547ad4b56a11600a5
康智超 <kangzhichao@3dbox.com>
2024-08-20 11:26:02 +0800

    添加初始化函数

--- include/74HC595_device.h
+++ include/74HC595_device.h
@@ -6,5 +6,5 @@ extern ShiftRegister74HC595<1> O1_74HC595_sr;
 extern ShiftRegister74HC595<1> O2_74HC595_sr;
 extern ShiftRegister74HC595<1> O3_74HC595_sr;
 extern ShiftRegister74HC595<1> O4_74HC595_sr;
-
+void IO_device_init();
 #endif // !__74HC959_DEVICE__
